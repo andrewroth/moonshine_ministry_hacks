d9d9c25b72e012e09559706c8e0d63fad69ebedd
Andrew Roth <andrewroth@gmail.com>
2009-11-13 05:18:53 -0500

    use deploy.rb.erb now

--- lib/generators/moonshine_ministry_hacks/templates/config/deploy.rb
+++
@@ -1,9 +0,0 @@
-load "#{File.dirname(__FILE__)}/../lib/capistrano_helper.rb"
-server "192.168.1.112", :app, :web, :db, :primary => true
-
-# multistage
-set :stages, %w(staging prod dev)
-require 'capistrano/ext/multistage'
-
-before "deploy:setup", :upload_certs
-after "deploy:setup", :destroy_config_files
